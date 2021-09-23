498b3bb7bedfbbdd98e180239715280a9e4d1ee3
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-09-23 17:24:20 -0500

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -8,9 +10,47 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void shouldReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("g]]"));
+    }
 
+    @Test
+    public void returnTrueIfDoubleBracketsClosed(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[g]]"));
+    }
+
+    @Test
+    public void returnTrueIfTripleBracketsClosed(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[g]]]"));
+    }
+
+    @Test
+    public void returnFalseForUnbalancedPairs(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[g]"));
+    }
+
+    @Test
+    public void returnTrueForBracketsOutOfOrderButStillPaired(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][[g]]"));
+    }
+
+    @Test
+    public void returnFalseForNull(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void returnTrueForVeryLongTest(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][]this is a really long string with spaces[[]][[[[]]]]hahahahahahahah[][][[]]"));
+    }
 }
+
+
+
+
+
